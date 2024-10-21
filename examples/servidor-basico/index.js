6fb699176eb551a0eede5dbee02dfe8e8c1abc2d
RamiroCovian <ramiro.covian@gmail.com>
2024-10-21 15:49:31 -0300

    feat: ejemplo servidor basico

--- examples/servidor-basico/index.js
+++ examples/servidor-basico/index.js
@@ -1,11 +1,11 @@
 // cargar libreria http
 const http = require('http');
 
-//definir un servidor
+//definir un servidor 'Content-type': 'text/plain': texto plano
 const server = http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-type': 'text/plain' });
+    response.writeHead(200, { 'Content-type': 'text/html' });
 
-    response.end('Wake up, Neo....');
+    response.end('Wake up, <b>Neo</b>....');
 })
 
 // arrancar el servidor
