c36270dcc64be98ca3ac84b2b0b9575e1a40adc0
RamiroCovian <ramiro.covian@gmail.com>
2024-11-06 17:49:48 -0300

    feat: manejo los errores

--- nodepop/app.js
+++ nodepop/app.js
@@ -1,10 +1,25 @@
 import express from 'express'
+import createError from 'http-errors'
+import logger from 'morgan'
+
+import * as homeController from './controllers/homeController.js'
 
 const app = express()
 
-app.get('/', (req, res, next) => {
-    console.log('Hola');
 
+app.use(logger("dev"))
+
+app.get('/', homeController.index)
+
+// Error handler
+app.use((req, res, next) => {
+    next(createError(404))
+})
+
+// Controlo los errores en el browser
+app.use((err, req, res, next) => {
+    console.log(err) // muestro el error por consola
+    res.send('Ocurrio un error: ' + err.message) // manejo el error
 })
 
 export default app
