be3332355b37e7ad038baf7f54d47caf5e8e63bc
RamiroCovian <ramiro.covian@gmail.com>
2024-11-06 17:05:39 -0300

    feat: uso debug para ver errores en consola

--- nodepop/server.js
+++ nodepop/server.js
@@ -1,16 +1,18 @@
 // Este modulo sera el que arrancara la app
 import http from 'node:http'
+import debugLib from 'debug'
 import app from './app.js'
 
-const port = 3000
+
+const debug = debugLib('nodepop:server')
+const port = process.env.port || 3000
 
 // Creo el http server
 const server = http.createServer(app)
 
-
+server.on('error', err => console.error(err))
 server.on('listening', () => {
-    console.log(`Servidor arrancado en puerto ${port}`);
-
+    debug(`Servidor arrancado en puerto ${port}`);
 })
 
 server.listen(port)
