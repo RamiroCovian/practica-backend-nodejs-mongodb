4bbe18fedb9e9d416e93b03c01cb6081b773da76
RamiroCovian <ramiro.covian@gmail.com>
2024-11-07 18:31:04 -0300

    feat: creo funcion para crear nuevo producto

--- nodepop/controllers/productsController.js
+++ nodepop/controllers/productsController.js
@@ -4,3 +4,25 @@ import createError from 'http-errors'
 export function index(req, res, next) {
     res.render('new-product')
 }
+
+
+export async function postNew(req, res, next) {
+    try {
+        const userId = req.session.userId
+        const { name, price, image, tags } = req.body
+
+        // Instancia de productos en memoria
+        const product = new Product({
+            name,
+            owner: userId,
+            price,
+            image,
+            tags,
+        })
+        // Guardo en DB
+        await product.save()
+        res.redirect('/')
+    } catch (err) {
+        next(err)
+    }
+}
