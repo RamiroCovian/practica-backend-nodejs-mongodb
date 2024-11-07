c0f73e70552d3ec304c8d314895b8d92355bdb53
RamiroCovian <ramiro.covian@gmail.com>
2024-11-07 08:53:52 -0300

    feat: creo funcion para logout

--- nodepop/controllers/loginController.js
+++ nodepop/controllers/loginController.js
@@ -31,3 +31,10 @@ export async function postLogin(req, res, next) {
         next(error)
     }
 }
+
+export function logout(req, res, next) {
+    req.session.regenerate(err => {
+        if (err) return next(err)
+        res.redirect('/')
+    })
+}
