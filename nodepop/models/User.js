48a90ca3261f31a9ddb158e26ad66cee1a770894
RamiroCovian <ramiro.covian@gmail.com>
2024-11-06 22:14:29 -0300

    feat: metodo para hashear password y compruebo si coinciden

--- nodepop/models/User.js
+++ nodepop/models/User.js
@@ -1,10 +1,21 @@
 import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
 
 // Creo la tabla
 const userSchema = new Schema({
     email: { type: String, unique: true },
     password: String,
 })
+
+// metodo estatico para hashear una password
+userSchema.statics.hashPassword = function (clearPassword) {
+    return bcrypt.hash(clearPassword, 7)
+}
+
+// Compruebo si la password coincide
+userSchema.methods.comparePassword = function (clearPassword) {
+    return bcrypt.compare(clearPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
