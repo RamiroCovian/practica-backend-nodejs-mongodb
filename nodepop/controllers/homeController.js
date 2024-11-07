e3821c87324b6a55f01fbe8d66e8859dd8df662c
RamiroCovian <ramiro.covian@gmail.com>
2024-11-07 09:50:01 -0300

    feat: hago peticion de productos segun usuario logado

--- nodepop/controllers/homeController.js
+++ nodepop/controllers/homeController.js
@@ -1,3 +1,8 @@
-export function index(req, res, next) {
+import Product from '../models/Product.js'
+export async function index(req, res, next) {
+    const userId = req.session.userId // Obtengo el userId
+    if (userId) {
+        res.locals.products = await Product.find({ owner: userId }) // Peticion de productos para el usuario logado
+    }
     res.render('Home')
 }
