8aec85c6717a9ed37d095ae36f2d35ea626a66f4
RamiroCovian <ramiro.covian@gmail.com>
2024-11-09 09:08:48 -0300

    feat: defino los campos requeridos y restricciones en la creacion de productos

--- nodepop/models/Product.js
+++ nodepop/models/Product.js
@@ -2,11 +2,24 @@ import mongoose, { Schema } from 'mongoose'
 
 // Defino Schema de los productos
 const productSchema = new Schema({
-    name: String,
-    owner: { type: Schema.Types.ObjectId, ref: 'User' },
-    price: Number,
-    image: String,
-    tags: [String]
+    name: {
+        type: String,
+        required: [true, 'Product name is required'],
+        trim: false
+    },
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    price: {
+        type: Number, required: [true, 'Product price is required'], min: [0, 'Price must be a positive number'], set: v => parseFloat(v).toFixed(2) // Me aseguro que el precio se guarde como un float con dos decimales
+    },
+    image: { type: String, },
+    tags: {
+        type: [String], required: [true, 'Product tags are required'], validate: {
+            validator: function (v) {
+                return v.every(tag => tag.includes('#'));
+            },
+            message: 'Each tag must contain the character #'
+        }
+    }
 });
 
 // Creo el modelo de productos
